05c042da462a6f695ba836160e662ddc37724e1b
Anthony Feeney <VigilantNighthawk@gmail.com>
2019-08-27 21:03:20 -0400

    Intial commit of sound generator.  Currently will generate a triangle based beep wave.  Will expose ability to set volume and type in updates by exposing appropriate parameters via accessors.

---
+++ sound.js
@@ -0,0 +1,28 @@
+class Beep{
+    
+    constructor(){
+        this.context = new AudioContext();
+        this.generator = this.context.createOscillator();
+        this.generator.type = "triangle";
+        this.gain = this.context.createGain();
+        this.generator.connect(this.gain).connect(this.context.destination);
+        this.playing = false;
+
+    }
+
+    play = () =>{
+        if(!this.playing){
+            this.playing = true;
+            this.generator.start(0);
+        }
+    }
+
+    stop = () =>{
+        if(this.playing){
+            this.playing = false;
+            this.gain.setValueAtTime(0,this.context.currentTime + 0.015);
+        }
+    }
+
+
+}
