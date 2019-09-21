6c8a986a3a71a101101fbe1271a6da384f165f6a
Anthony Feeney <VigilantNighthawk@gmail.com>
2019-09-21 18:19:31 -0400

    Added ability to load font as a Uint8Array into the emulator.

--- chip8.js
+++ chip8.js
@@ -1,127 +1,6 @@
 
 export {Chip8, VRam};
-//chip 8 8 bit system sprites
-/** @todo -- consider refactoring into separate data file */
-const sprites8Bit = new Uint8Array(60);
-sprites8Bit.append = function(){
-    let index = -1;
-    return function(row){
-       index++;
-       this[index] = row; 
-    }
-}();
 
-//zero
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-
-//one
-sprites8Bit.append(0x20);
-sprites8Bit.append(0x60);
-sprites8Bit.append(0x20);
-sprites8Bit.append(0x20);
-sprites8Bit.append(0x70);
-
-//two
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-
-//three
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0xF0);
-
-//four
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0x10);
-
-//five
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0xF0);
-
-//six
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-
-//seven
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0x20);
-sprites8Bit.append(0x40);
-sprites8Bit.append(0x40);
-
-//eight
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-
-//nine
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x10);
-sprites8Bit.append(0xF0);
-
-//A
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-
-//B
-sprites8Bit.append(0xE0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xE0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xE0);
-
-//C
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-
-//D
-sprites8Bit.append(0xE0);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0x90);
-sprites8Bit.append(0xE0);
-
-//E
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-
-//F
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0xF0);
-sprites8Bit.append(0x80);
-sprites8Bit.append(0x80);
 
 const programOffset = 0x200;
 export default class Chip8{
@@ -139,7 +18,8 @@ export default class Chip8{
 
         this.sound = null;
 
-       
+        this.chip8Font = null;
+        this.superChipFont = null;
 
         this._clockSpeed = 400; //default to 400hz
 
@@ -152,6 +32,11 @@ export default class Chip8{
         this._callbacks = new Array();
     }
 
+    loadChip8Font(font){
+        this.chip8font = font;
+    }
+
+
     loadRom(rom){
         if(!(rom instanceof Uint8Array)){
             throw new Error("Roms must be an Uint8Array");
@@ -160,7 +45,10 @@ export default class Chip8{
         
         this.ram = new Uint8Array(4096);
         //copy 8 bit fonts into ram at start
-        this.ram.set(sprites8Bit);    
+        if(!(this.chip8Font instanceof Uint8Array)){
+            throw new Error("Chip 8 font is must an instance of Uint8Array");
+        }
+        this.ram.set(this.chip8font);    
 
         //copy rom into ram
         this.ram.set(rom.slice(0,4096),programOffset);
